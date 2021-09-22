1086cd66d66aa6c7a76bf4e4b374c586446916eb
ShonBC <scortes3@umd.edu>
2021-09-22 14:59:45 -0700

    All functions properly work and model is trained.

--- Homework_1/808A_HW1_p1.py
+++ Homework_1/808A_HW1_p1.py
@@ -12,80 +12,109 @@ https://college.cengage.com/mathematics/brase/understandable_statistics/7e/stude
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy.core.records import array
 import pandas as pd
 import sklearn.linear_model
 
 def ShowData(x, y, color):
+    """Plots data
+
+    Args:
+        x ([type]): Input data values
+        y ([type]): Output data values
+        color ([str]): Color of data points
+    """
 
     plt.plot(x, y, color)
     plt.xlabel("Target Values")
     plt.ylabel("Features")
 
-def ImportData(sales_data, col_title):
+def SepTestData(x_test):
+    """Removes the 'nan' type data from test data list
 
-    x = pd.DataFrame(sales_data, columns=[col_title])
-    train_data = [] 
-    test_data =[]
+    Args:
+        x_test ([list]): Test data to train model
+
+    Returns:
+        [list]: Test data with 'nan' data values removed
+    """
+
+    test_data = []
     i = 0
 
-    while i <= len(list(x.T)) - 1:
+    while i <= len(list(x_test.T)) - 1:
 
-        if i < 19:
-            train_data.append(x.T.values[0][i])
-        if i >= 19:
-            test_data.append(x.T.values[0][i])
+        if not np.isnan(x_test.T.values[0][i]):
+            
+            test_data.append(x_test.T.values[0][i])
+            
+        else:
+
+            pass            
 
         i += 1
     
-    return train_data, test_data
+    return test_data
 
-def TestModel(x_test):
+def TestModel(x_test, model):
+    """Tests the given model using the input test data.
+
+    Args:
+        x_test ([list]): Input data to test model
+        model ([type]): Trained model to test
+    """
 
     for data in x_test:
 
-        x_new   = [[data]]
-        print(model2.predict(x_new))
+        x_new = [[data]]
+        print(f'Test Data Value: {data} Model Prediction: {model.predict(x_new)}')
 
-if __name__ == '__main__':
-    # Load the data
-    sales_data = pd.read_excel('Homework_1/mlr05.xls', 'Mlr05')
-    # x1 = pd.DataFrame(sales_data, columns=['X1'])
-    x1, x_test = ImportData(sales_data, 'X1')
-    x2, x2_test = ImportData(sales_data, 'X2')
-    # x2 = pd.DataFrame(sales_data, columns=['X2'])
-    # x3 = pd.DataFrame(sales_data, columns=['X3'])
-    # x4 = pd.DataFrame(sales_data, columns=['X4'])
-    # x5 = pd.DataFrame(sales_data, columns=['X5'])
-    # x6 = pd.DataFrame(sales_data, columns=['X6'])
-    # print(x1)
-    # print(x2)
+def LinearReg(x1, x, x_test, color):
+    """Show plot of the train data. Train the linear model using Linear Regression and use test data to test the model.
 
-    # Prepare the data
-
-
+    Args:
+        x1 ([type]): Target output data
+        x ([type]): Input data
+        x_test ([list]): Input data to test the model
+        color ([str]): color to use for plotting model
+    """
     # Visualize the data
-    # ShowData(x2, x1, 'ro')
-    # ShowData(x3, x1, 'bo')
-    # ShowData(x4, x1, 'go')
-    # ShowData(x5, x1, 'co')
-    # ShowData(x6, x1, 'mo')
-    plt.show()
+    ShowData(x2, x1, color)
 
     # Select a linear model
-    model2 = sklearn.linear_model.LinearRegression()
-    # model3 = sklearn.linear_model.LinearRegression()
-    # model4 = sklearn.linear_model.LinearRegression()
-    # model5 = sklearn.linear_model.LinearRegression()
-    # model6 = sklearn.linear_model.LinearRegression()
-
+    model = sklearn.linear_model.LinearRegression()
+    
     # # Train the model
-    model2.fit(x2,x1)
-    # model3.fit(x3,x1)
-    # model4.fit(x4,x1)
-    # model5.fit(x5,x1)
-    # model6.fit(x6,x1)
+    model.fit(x, x1)
 
     # Make a prediction
-    # X_new = [[4]]
-    # print(model2.predict([x2_test]))
-    TestModel(x2_test)
+    x_test = SepTestData(x_test)
+    TestModel(x_test, model)
+    plt.show()
+
+if __name__ == '__main__':
+   
+    # Load and prepare the data
+    sales_data = pd.read_excel('Homework_1/mlr05.xls', 'Mlr05')
+    x1 = pd.DataFrame(sales_data, columns=['X1'])
+    x2 = pd.DataFrame(sales_data, columns=['X2'])
+    x2_test = pd.DataFrame(sales_data, columns=['X2_test'])
+    x3 = pd.DataFrame(sales_data, columns=['X3'])
+    x3_test = pd.DataFrame(sales_data, columns=['X3_test'])
+    x4 = pd.DataFrame(sales_data, columns=['X4'])
+    x4_test = pd.DataFrame(sales_data, columns=['X4_test'])
+    x5 = pd.DataFrame(sales_data, columns=['X5'])
+    x5_test = pd.DataFrame(sales_data, columns=['X5_test'])
+    x6 = pd.DataFrame(sales_data, columns=['X6'])
+    x6_test = pd.DataFrame(sales_data, columns=['X6_test'])
+
+    print("Model for 'X2' data:")
+    LinearReg(x1, x2, x2_test, 'ro')
+    print("Model for 'X3' data:")
+    LinearReg(x1, x3, x3_test, 'bo')
+    print("Model for 'X4' data:")
+    LinearReg(x1, x4, x4_test, 'go')
+    print("Model for 'X5' data:")
+    LinearReg(x1, x5, x5_test, 'co')
+    print("Model for 'X6' data:")
+    LinearReg(x1, x6, x6_test, 'mo')    
