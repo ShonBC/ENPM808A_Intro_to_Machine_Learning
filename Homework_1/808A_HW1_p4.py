1924320cfa2f44cdb8c805fa398b283efff610e4
ShonBC <scortes3@umd.edu>
2021-09-22 19:15:09 -0700

    Finalized perceptron learning algoorithm.

--- Homework_1/808A_HW1_p4.py
+++ Homework_1/808A_HW1_p4.py
@@ -27,55 +27,148 @@ The algorithm above is a variant of the so-called Adaline (Adaptive LinearNeuron
 for perceptron learning.
 """
 
-import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import sklearn.linear_model
+from numpy import linalg, random
 
-def TestPoint(target_function, x_point):
+class Perceptron:
+    """Perceptron Algorithm class
+    """
 
-    pass
+    def __init__(self):
+        """Init with random weight vector 'w' and n
+        """
 
-def Perceptron(train_data, n):
-  
-    it = 0
-    max_updates = 1000
+        self.w = random.rand(2)
+        self.n = 100
 
-    while it <= max_updates:
+    def Output(self, x):
+        """Calculates the output for a given input data value.
 
-        idx = np.random.randint(0, high= 1000)
-        x = train_data[idx][0]
-        y = train_data[idx][1]
-        w_t = 0
-        s = 0
+        Args:
+            x (list): input data (x, y) points
 
-        if s * y > 1:
-            pass
+        Returns:
+            [int]: 1 if the point is properly classified, -1 is point is misclassified
+        """
 
+        y = x[0] * self.w[0] + x[1] * self.w[1]
+
+        if y >= 0:
+            return 1
         else:
+            return -1
 
-            w_t1 = w_t + n(y - s) * x
+    def Weight(self, x, err):
+        """Recalculates the weight vector based off the new error.
 
-        it += 1
+        Args:
+            x (list): input data (x, y) points
+            err (int): difference in predicted vs actual output (y(t)−s(t))
+        """
+
+        self.w[0] += self.n * err * x[0]
+        self.w[1] += self.n * err * x[1]
+
+    def Train(self, data):
+        """Train the model no more than 1000 times
+
+        Args:
+            data (list): List of (x, y) pairs for input and output data
+        """
+
+        it = 0 
+        max_updates = 1000
+        iterating = True
+
+        while iterating:
+
+            total_err = 0
+            for x in data:
+
+                out = self.Output(x)
+
+                if x[2] != out:
+
+                    err = x[2] - out
+                    self.Weight(x, err)
+                    total_err += abs(err)
+
+            it += 1
+
+            if total_err == 0 or it >= max_updates:
+                print(f'Iterated {it} iterations.')
+                iterating = False   
+        
+
+    def Data(total_samples):
+        """Generates random data points to either test or train with.
+
+        Args:
+            total_samples (int): Total number of samples to generate
+
+        Returns:
+            [list]: List of data points (x,y)
+        """
+
+        total_samples = total_samples / 2
+        total_samples = int(total_samples)
+        x1 = random.rand(total_samples)
+        y1 = random.rand(total_samples)
+        x2 = random.rand(total_samples)
+        y2 = random.rand(total_samples)
+
+        data_set = []
+
+        for i in range(len(x1)):
+
+            data_set.append([x1[i], y1[i], 1])
+            data_set.append([x2[i], y2[i], -1])
+        
+        return data_set
+
+    def main(n = 100):
+        """Main function that trains data then tests the model with random test data.
+
+        Args:
+            n (int, optional): Weight adjustment parameter. Defaults to 100.
+        """
+
+        train_data = Perceptron.Data(100)
+        test_data = Perceptron.Data(10000)
+        perceptron = Perceptron()
+        perceptron.n = n
+        perceptron.Train(train_data)
+
+        for x in test_data:
+
+            err = perceptron.Output(x) 
+
+            if err == 1: # Properly classified
+
+                plt.plot(x[0], x[1], 'go')
+
+            else: # Incorrectly classified
+
+                plt.plot(x[0], x[1], 'ro')
+
+        n = linalg.norm(perceptron.w)
+        ww = perceptron.w / n
+        ww1 = [ww[1], -ww[0]]
+        ww2 = [-ww[1], ww[0]]
+        plt.plot([ww1[0], ww2[0]], [ww1[1], ww2[1]], '-k')
+        plt.show()
 
 if __name__ == '__main__':
 
-    # Generate random data points
-    train_data = np.random.randint(0, high=1000, size=(2, 100)).T
-    # print(train_data[1][0], train_data[1][1])
-    # print(train_data)
+    # Part (a)
+    Perceptron.main()
 
-    test_data = np.random.randint(0, high=1000, size=(2, 10000)).T
+    # Part (b)
+    Perceptron.main(1)
 
-    n = 100
+    # Part (c)
+    Perceptron.main(.01)
 
-    # Define target function
-    m = 12
-    x = np.array(range(0, 1000))
-    b = 125
-    y = m*x+b
-    plt.plot(x, y)
-    plt.title('Target Function')
-    plt.show()  
-    # Perceptron()
+    # Part (d)
+    Perceptron.main(.0001)
